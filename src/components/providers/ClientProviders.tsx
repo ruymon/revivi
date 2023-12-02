5ced50b82d94c4c329fde3709701746cf6ea2355
Ruy Monteiro <ruystfrancis@gmail.com>
2023-12-02 15:50:32 -0300

    feat: Auth

---
+++ src/components/providers/ClientProviders.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactNode } from "react";
+import { Toaster } from "../ui/Toaster";
+
+interface ClientProvidersProps {
+  children: ReactNode;
+}
+
+export function ClientProviders({ children }: ClientProvidersProps) {
+  return (
+    <NextThemesProvider
+      themes={['light', 'dark']}
+      attribute="class"
+      defaultTheme="light"
+      disableTransitionOnChange
+      enableSystem={false}
+    >
+      {children}
+      <Toaster />
+    </NextThemesProvider>
+  )
+}
