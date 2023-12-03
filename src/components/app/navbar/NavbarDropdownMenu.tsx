b868361ab24bd15e767712e10739f858cd8c8b24
Ruy Monteiro <ruystfrancis@gmail.com>
2023-12-03 04:47:08 -0300

    hotfix: file locations

---
+++ src/components/app/navbar/NavbarDropdownMenu.tsx
@@ -0,0 +1,35 @@
+import { signOut } from '@/actions/authActions';
+import { ThemeSwitcher } from '@/components/ThemeSwitcher';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/DropdownMenu';
+import { Menu } from 'lucide-react';
+
+interface NavbarDropdownMenuProps {};
+
+export function NavbarDropdownMenu({}: NavbarDropdownMenuProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className='data-[state=open]:text-accent-foreground transition-colors text-muted-foreground p-2 rounded-md hover:bg-accent hover:text-accent-foreground'>
+        <Menu />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end' sideOffset={8}>
+        <DropdownMenuItem>
+          <ThemeSwitcher>
+            Alternar exibição
+          </ThemeSwitcher>
+        </DropdownMenuItem>
+        <DropdownMenuItem>Configurações</DropdownMenuItem>
+        <DropdownMenuItem>Sobre</DropdownMenuItem>
+        <DropdownMenuItem asChild className='p-0'>
+          <form action={signOut} >
+            <button className='w-full h-full text-left px-2 py-1.5'>Sair</button>
+          </form>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
